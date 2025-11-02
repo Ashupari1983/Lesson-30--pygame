9c5a09722b6554f7dd6798036ed709da0dd19ad5
Ashupari1983 <ashupari123@gmail.com>
2025-11-02 15:10:44 +0530

    abcdefghijklmnopqrstuvwxyz

--- main.py
+++ main.py
@@ -7,9 +7,15 @@ display_surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Pygame Window")
 background_img = pygame.transform.scale(pygame.image.load('bg.jpg').convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
 donut = pygame.transform.scale(pygame.image.load('donut.png').convert_alpha(), (200, 200))
-donu_rect = donut.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2-30))
+Candy = pygame.transform.scale(pygame.image.load('candy.png').convert_alpha(), (200, 200))
+
+
+Candy_rect = Candy.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 - 100))
+donut_rect = donut.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 100))
+
 text_font = pygame.font.Font(None, 36).render("Sweet!", True, pygame.Color('pink'))
 text_rect = text_font.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2 + 110))
+
 clock = pygame.time.Clock()
 
 while True:
@@ -18,7 +24,8 @@ while True:
             pygame.quit()
         
     display_surface.blit(background_img, (0, 0))
-    display_surface.blit(donut, donu_rect)  
+    display_surface.blit(donut, donut_rect)  
+    display_surface.blit(Candy, Candy_rect)
     display_surface.blit(text_font, text_rect)
 
     pygame.display.flip()
